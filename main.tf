822e1d107d88353e0bfdc75e04fa587d6e5adec0
Raghu K <raghuk.vit@gmail.com>
2025-12-12 07:28:55 +0530

    Roboshop project

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "ami" {
   ami                    = "ami-09c813fb71547fc4f"
   instance_type          = "t3.small"
   vpc_security_group_ids = ["sg-09663d91a4fca31c9"]
@@ -8,7 +8,24 @@ resource "aws_instance" "web" {
   }
 }
 
+resource "null_resource" "ansible" {
 
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = aws_instance.ami.private_ip
+    }
+
+    inline = [
+      "sudo dnf install python3.13-pip -y",
+      "sudo pip3.11 install ansible"
+    ]
+
+  }
+
+}
 
 
 
